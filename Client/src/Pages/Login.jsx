0073a642316f57a3575098f7a3c21c31eaccc2bb
Gary H <3rdeye.ghj@gmail.com>
2023-09-05 11:20:33 -0700

    Basic skeleton for login page built

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -4,7 +4,26 @@ import React from "react";
 function Login () {
   return (
     <>
-      <h1>Whisper Chat App</h1>
+    <div className="login-container">
+      <h1 className="login-header">Chat Room Log In</h1>
+      <input type="text" placeholder="Username"/>
+      <br />
+      <input type="text" placeholder="Password"/>
+      <br />
+      <button className="login-btn">Log In</button>
+      <br />
+      <a href="">Forgot Password?</a>
+    </div>
+    <div className="signup-container">
+      <h1 className="">Greetings!</h1>
+
+      <h2 className="join">Join the conversation</h2>
+      <br />
+      <button className="signup-btn">Sign up</button>
+    </div>
+
+      
+      
     </>
   )
 }
