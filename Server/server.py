3bfc6049b55e79e04744c0144a874a5f90fa10ff
Gary H <3rdeye.ghj@gmail.com>
2023-09-18 13:32:46 -0700

    installed SQLAlchemy with config and building the data base

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,22 @@
 from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/chat_app_database'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
 
 @app.route('/')
 def hello():
