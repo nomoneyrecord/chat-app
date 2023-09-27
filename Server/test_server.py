e81249f562b23c4fcfc224989bf350ceb846b99b
Gary H <3rdeye.ghj@gmail.com>
2023-09-27 15:03:22 -0700

    Made adjustments to app context and unittests are working

--- Server/test_server.py
+++ Server/test_server.py
@@ -9,36 +9,40 @@ class ServerTestCase(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
         self.app = app.test_client()
         with app.app_context():
-            db.create_all() 
+            db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
 
     def test_login(self):
-        user = User(username="testuser", password="testpass")
-        db.session.add(user)
-        db.session.commit()
+        with app.app_context():
+            user = User(username="testuser", password="testpass")
+            db.session.add(user)
+            db.session.commit()
 
-        response = self.app.post('/api/users', data=json.dumps(dict(
-            username="testuser",
-            password="testpass"
-        )), content_type='application/json', follow_redirects=True)
+            response = self.app.post('/api/users', data=json.dumps(dict(
+                username="testuser",
+                password="testpass"
+            )), content_type='application/json', follow_redirects=True)
 
-        self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.status_code, 201)
 
     def test_create_message(self):
-        user = User(username="testuser", password="testpass")
-        db.session.add(user)
-        db.session.commit()
+        with app.app_context():
+            user = User(username="testuser", password="testpass")
+            db.session.add(user)
+            db.session.commit()
 
-        response = self.app.post('/api/messages', data=json.dumps(dict( 
-          user_id=user.id,
-          message="Hello, world!"
-        )), content_type='application/json', follow_redirects=True) 
+            response = self.app.post('/api/messages', data=json.dumps(dict(
+              user_id=user.id,
+              message="Hello, world!"
+            )), content_type='application/json', follow_redirects=True)
 
-        self.assertEqual(response.status_code, 201)
-        self.assertIn(b"Hello, world!", response.data)
+            self.assertEqual(response.status_code, 201)
+            self.assertIn(b"Hello, world!", response.data)
 
 if __name__ == '__main__': 
     unittest.main()
+
