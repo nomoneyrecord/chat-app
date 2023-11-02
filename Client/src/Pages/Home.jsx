59009645805a0f9e31fc8a334762e5795db0d11b
Gary H <3rdeye.ghj@gmail.com>
2023-11-02 14:27:16 -0700

    Reverted code due to issues with trying to fix sockets errors in deployment

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -23,11 +23,10 @@ function Home({ onLogout }) {
         console.error("Error fetching messages:", error);
       });
 
-    const socketURL =
-      import.meta.env.VITE_REACT_APP_SOCKETIO_URL || "http://127.0.0.1:8000";
-    socketRef.current = io(socketURL);
-
-    console.log("Socket URL:", socketURL);
+      const socketURL = process.env.REACT_APP_SOCKETIO_URL || "http://127.0.0.1:8000";
+      socketRef.current = io(socketURL);
+      
+      
 
     socketRef.current.on("receive_message", (message) => {
       if (!messages.some((msg) => msg.id === message.clientId)) {
@@ -113,11 +112,7 @@ function Header({ onLogout }) {
       style={{ backgroundColor: "#444", height: "60px" }}
     >
       <div className="text-white">Chat App Logo</div>
-      <button
-        className="btn-light"
-        style={{ textAlign: "center", display: "block" }}
-        onClick={handleLogoutClick}
-      >
+      <button className="btn-light" style={{textAlign: 'center', display: 'block'}} onClick={handleLogoutClick}>
         Logout
       </button>
     </div>
@@ -163,7 +158,7 @@ function ChatWindow({
 
   useEffect(() => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
+  }, [messages])
 
   const handleSubmit = (e) => {
     e.preventDefault();
